eef548b7828119462de9100259b5dde4d533ad6e
bepo03 <wjsdbsgus45@gmail.com>
2026-06-30 20:45:06 +0900

    init project

---
+++ src/main/java/com/backtoback/reseat/ReSeatApplication.java
@@ -0,0 +1,13 @@
+package com.backtoback.reseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReSeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReSeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backtoback/reseat/ReSeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backtoback.reseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReSeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
