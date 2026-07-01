f0d52bf7d03179ec50c0fa61a7e69be4a1f69d85
LFCKJ <k71689@naver.com>
2026-07-01 21:32:32 +0900

    fix: UserRepository Generic type error

--- src/main/java/com/backtoback/reseat/domain/user/repository/UserRepository.java
+++ src/main/java/com/backtoback/reseat/domain/user/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     //이메일로 유저 정보 조회 (로그인 시 사용)
     Optional<User> findByEmail(String email);
 
