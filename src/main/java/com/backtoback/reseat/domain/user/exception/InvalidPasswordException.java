25538674c874c1230fd787319afcfbdb63c1ae8d
siho <dolphin@kakao.com>
2026-07-07 06:27:16 +0900

    refactor: BusinessException 기반 예외 처리 구조 정리

--- src/main/java/com/backtoback/reseat/domain/user/exception/InvalidPasswordException.java
+++ src/main/java/com/backtoback/reseat/domain/user/exception/InvalidPasswordException.java
@@ -1,7 +1,10 @@
 package com.backtoback.reseat.domain.user.exception;
 
-public class InvalidPasswordException extends RuntimeException {
+import com.backtoback.reseat.global.exception.BusinessException;
+import com.backtoback.reseat.global.exception.ErrorCode;
+
+public class InvalidPasswordException extends BusinessException {
     public InvalidPasswordException(String message) {
-        super(message);
+        super(ErrorCode.UNAUTHORIZED, message);
     }
 }
