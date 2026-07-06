c990e9868ab0d06648241f375611bbc8092b4793
LFCKJ <k71689@naver.com>
2026-07-06 22:03:36 +0900

    feat: 로컬 및 운영 환경용 통합 docker-compose.yml 구축

--- src/main/java/com/backtoback/reseat/global/config/RedisConfig.java
+++ src/main/java/com/backtoback/reseat/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -18,9 +19,22 @@ public class RedisConfig {
     @Value("${spring.data.redis.port:6379}")
     private int port;
 
+    @Value("${spring.data.redis.passwrod:}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host, port);
+
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setHostName(host);
+        redisConfiguration.setPort(port);
+
+        //비밀번호가 비어있지 않을 때
+        if(!password.isEmpty()){
+            redisConfiguration.setPassword(password);
+        }
+
+        return new LettuceConnectionFactory(redisConfiguration);
     }
 
     @Bean
