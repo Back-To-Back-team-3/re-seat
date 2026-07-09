da7f8611920c10fc0d1528169253b6e929975de9
siho <dolphin@kakao.com>
2026-07-10 06:21:19 +0900

    refactor: Toss confirm/cancel 응답을 공통 DTO로 통합

--- src/main/java/com/backtoback/reseat/domain/payment/pg/toss/dto/response/TossPaymentResponse.java
+++ src/main/java/com/backtoback/reseat/domain/payment/pg/toss/dto/response/TossPaymentResponse.java
@@ -1,16 +1,19 @@
 package com.backtoback.reseat.domain.payment.pg.toss.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+// 토스 결제 단건 조회 응답 중 결제 상태 동기화에 필요한 필드만 매핑한다.
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TossCancelResponse {
+public class TossPaymentResponse {
 
     private String paymentKey;
+    private String orderId;
     private String status;
+    private Integer totalAmount;
+    private String approvedAt;
+    private String method;
 }
