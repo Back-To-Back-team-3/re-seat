1be9acbdea7f2061dcca3f883c038583db9930f6
LFCKJ <k71689@naver.com>
2026-07-27 20:48:23 +0900

    chore: 서버시간 kst로 동기화 및 cd.yml파일 수정, docker-compose.yml 수정, prometheus.yml파일 위치 수정

--- src/main/java/com/backtoback/reseat/ReSeatApplication.java
+++ src/main/java/com/backtoback/reseat/ReSeatApplication.java
@@ -1,13 +1,22 @@
 package com.backtoback.reseat;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
 public class ReSeatApplication {
 
+
+    @PostConstruct
+    public void init() {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(ReSeatApplication.class, args);
     }
