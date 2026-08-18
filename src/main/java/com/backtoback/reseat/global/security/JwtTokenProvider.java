85c19d5ab12d5c1a664b18a7aad8e1e5553c0b4a
LFCKJ <k71689@naver.com>
2026-08-18 23:06:44 +0900

    fix(infra) : 리뷰 반영 test 시크릿 키 githubAcitonsSecrets에 저장

--- src/main/java/com/backtoback/reseat/global/security/JwtTokenProvider.java
+++ src/main/java/com/backtoback/reseat/global/security/JwtTokenProvider.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import com.backtoback.reseat.domain.verification.service.CustomUserDetailsService;
+import com.backtoback.reseat.domain.user.auth.service.CustomUserDetailsService;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
