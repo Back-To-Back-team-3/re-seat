73a7a36763484ce04ea2cafae1fe259cc16d4c53
R1NN <r1nn650t@gmail.com>
2026-09-06 22:32:26 +0900

    test(seat-inventory): refund soldAt 초기화 검증 강화

--- src/test/java/com/backtoback/reseat/domain/seatinventory/entity/GameSeatTest.java
+++ src/test/java/com/backtoback/reseat/domain/seatinventory/entity/GameSeatTest.java
@@ -108,6 +108,7 @@ class GameSeatTest {
         @DisplayName("SOLD 좌석은 AVAILABLE로 돌아가고 판매 시각이 초기화된다")
         void success() {
             GameSeat seat = seatWith(GameSeatStatus.SOLD);
+            seat.sell(); // soldAt이 실제로 채워진 상태를 먼저 만든다
 
             seat.refund();
 
